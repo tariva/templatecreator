946ccbee63e9f8f605d15479142531b361bda577
Ivan Tarskich <ivan.tarskich@edu.fh-joanneum.at>
2023-09-08 14:32:31 +0200

    build script and processed files

--- index.js
+++ index.js
@@ -3,20 +3,35 @@ const fs = require("fs");
 const XLSX = require("xlsx");
 const iconv = require("iconv-lite");
 
-const directoryPath = "./testfiles/"; // Current directory. Change this if needed.
-// delete and create output folder
-fs.rmdirSync("outputfiles", { recursive: true });
-fs.mkdirSync("outputfiles");
+const directoryPath = "./testfiles/";
+const outputPath = "./outputfiles/";
+// Delete and recreate the outputfiles directory
+
+try {
+  fs.rmdirSync("outputfiles", { recursive: true });
+  console.log("Successfully deleted the outputfiles directory.");
+} catch (err) {
+  console.error("Error while deleting the outputfiles directory.", err);
+}
+
+try {
+  fs.mkdirSync("outputfiles");
+  console.log("Successfully created the outputfiles directory.");
+} catch (err) {
+  console.error("Error while creating the outputfiles directory.", err);
+  return; // Exit if there's an error
+}
 
 // Read all files in the directory
 fs.readdir(directoryPath, (err, files) => {
   if (err) {
     return console.error("Unable to scan directory: " + err);
   }
-
+  const wb = XLSX.utils.book_new();
   let data = [];
   let keys = [];
   let values = [];
+  let processedFiles = [];
   files.forEach((file) => {
     // Ignore files with extensions
     if (!file.includes(".")) {
@@ -24,7 +39,7 @@ fs.readdir(directoryPath, (err, files) => {
       const contentBuffer = fs.readFileSync(directoryPath + file);
       const content = iconv.decode(contentBuffer, "win1252");
       const lines = content.split("\n");
-
+      processedFiles.push(file);
       for (let line of lines) {
         /*
         In this regex:
@@ -46,13 +61,18 @@ fs.readdir(directoryPath, (err, files) => {
       }
       data = [keys, values];
     }
-  });
+    if (data.length > 0) {
+      // Create worksheet
+      const ws = XLSX.utils.aoa_to_sheet(data);
 
-  // Write data to Excel
-  const wb = XLSX.utils.book_new();
-  const ws = XLSX.utils.aoa_to_sheet(data);
-  XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-  // write to output folder
+      // Write data to Excel
+
+      XLSX.utils.book_append_sheet(wb, ws, file);
+      // write to output folder
+    }
+  });
   XLSX.writeFile(wb, "outputfiles/output.xlsx");
   console.log("Excel file created as output.xlsx");
+  // After processing all files, write the names of processed files to a text file
+  fs.writeFileSync("processed_files.txt", processedFiles.join("\n"));
 });
